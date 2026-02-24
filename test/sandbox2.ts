244921ad1753b888b3bce04911fb08e1242ea684
finch ren <344919881@qq.com>
2026-02-24 19:08:08 +0800

    chore(test): remove obsolete sandbox test files

--- test/sandbox2.ts
+++
@@ -1,9 +0,0 @@
-import { getClient } from './init';
-
-async function main() {
-  const client = await getClient();
-  const response = await client.userList.getBlueVerifiedFollowers({ userId: '44196397' });
-  console.log(JSON.stringify(response.data.raw, null, 2));
-}
-
-main().catch(console.error);
